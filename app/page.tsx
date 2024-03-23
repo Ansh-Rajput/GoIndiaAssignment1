c550f76909c69822281c4e35eefc075a67f0ea1b
Ansh-Rajput <anshravikrish425317@gmail.com>
2024-03-23 18:38:02 +0530

    fixed suspense boundry bug

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,17 @@
+"use client";
 import { Disscussion } from "@/components/DisscussionComponents/Disscussion";
 import { MarketStories } from "@/components/MarketStoryComponents/MarketStories";
+import { Suspense } from "react";
 
 export default function Home() {
   return (
     <main className="grid max-h-screen grid-cols-12 relative">
-      <Disscussion/>
-      <MarketStories/>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Disscussion />
+      </Suspense>
+      <Suspense fallback={<div>Loading...</div>}>
+        <MarketStories />
+      </Suspense>
     </main>
   );
 }
