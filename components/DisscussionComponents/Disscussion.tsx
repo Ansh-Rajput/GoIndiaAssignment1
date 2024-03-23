fd9df0ae7872d97e76969c50d068a4d511629686
Ansh-Rajput <anshravikrish425317@gmail.com>
2024-03-23 18:46:13 +0530

    fixed: typo

--- components/DisscussionComponents/Disscussion.tsx
+++ components/DisscussionComponents/Disscussion.tsx
@@ -7,7 +7,7 @@ export const Disscussion = () => {
   return (
     <div 
         className="w-full fixed bg-white xl:static h-full col-span-12 xl:col-span-9 flex-col"
-        style={{display:(searchParams.get("page") === "stories" ? "-1" : "10" )}}
+        style={{zIndex:(searchParams.get("page") === "stories" ? "0" : "10" )}}
     >
       <h1 className="hidden xl:flex h-[50px] text-lg text-red-500 bg-slate-300 justify-center items-center my-0 w-[calc(100%-100px)] m-auto">Disscussion</h1>
       <div className="overflow-y-scroll h-[calc(100vh-70px)] hideScrollBar">
