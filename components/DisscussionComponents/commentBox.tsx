071248595dd2d3e3852a8db3a6243966bbcf4ca4
Ansh-Rajput <anshravikrish425317@gmail.com>
2024-03-23 20:27:55 +0530

    fixed: changed width of comment box

--- components/DisscussionComponents/commentBox.tsx
+++ components/DisscussionComponents/commentBox.tsx
@@ -7,9 +7,9 @@ interface CommentBoxProps {
   comment: string;
   sector: string;
 }
-export const CommentBox = ({ name, comment, sector }: CommentBoxProps) => {
+export const CommentBox = ({name, comment, sector }: CommentBoxProps) => {
   return (
-    <div className="w-[calc(100%-100px)] m-auto grid grid-cols-12 shadow-md rounded-md max-h-[calc(100vh/4)] my-5">
+    <div className="md:w-[calc(100%-100px)] m-auto grid grid-cols-12 shadow-md rounded-md max-h-[calc(100vh/4)] my-5">
       <div className="flex justify-center col-span-2">
         <FaUserCircle size={25} />
       </div>
@@ -17,10 +17,12 @@ export const CommentBox = ({ name, comment, sector }: CommentBoxProps) => {
         <div className="flex gap-2">
           {name}
           <span className="text-white bg-blue-600 rounded-xl text-center flex items-center px-1 text-xs">
-            {sector}
+           {sector}
           </span>
         </div>
-        <div className="text-sm  line-clamp-3 overflow-hidden">{comment}</div>
+        <div className="text-sm  line-clamp-3 overflow-hidden">
+          {comment}
+        </div>
         <div className="flex justify-between py-1">
           <div className="flex gap-1 justify-center items-center">
             <BiHeart />
